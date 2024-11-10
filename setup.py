b5fd70da0ba8985d1b644f087d40c663fb92fb16
jackburke12 <jburkey2499@gmail.com>
2024-11-10 12:05:42 -0500

    update package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'booklover',
+    version = '0.1.0',
+    author = 'Jack Burke',
+    author_email = 'jpb2uj@virginia.edu',
+    packages = ['booklover'],
+    url = 'http://pypi.python.org/pypi/PackageName/',
+    license = 'MIT.txt',
+    description = 'Rate and keep track of books you have read with booklover',
+    long_description = open('README.md').read(),
+)
